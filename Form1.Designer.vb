30ea14d6c975a39af28955412854ee1a84e6e120
Kipkemboi <Kipkemboi@DESKTOP-4F5LS9M>
2024-02-11 13:38:30 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        txtName = New TextBox()
+        txtPhone = New TextBox()
+        txtEmail = New TextBox()
+        txtReg = New TextBox()
+        ComboBox2 = New ComboBox()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        DateTimePicker1 = New DateTimePicker()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        Label1.Location = New Point(33, 85)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(46, 17)
+        Label1.TabIndex = 0
+        Label1.Text = "Name"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        Label2.Location = New Point(33, 146)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(57, 17)
+        Label2.TabIndex = 1
+        Label2.Text = "Gender"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        Label3.Location = New Point(33, 206)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(49, 17)
+        Label3.TabIndex = 2
+        Label3.Text = "Phone"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        Label4.Location = New Point(33, 262)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(45, 17)
+        Label4.TabIndex = 3
+        Label4.Text = "Email"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        Label5.Location = New Point(367, 85)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(58, 17)
+        Label5.TabIndex = 4
+        Label5.Text = "Reg No"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        Label7.Location = New Point(367, 206)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(56, 17)
+        Label7.TabIndex = 6
+        Label7.Text = "Course"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        Label8.Location = New Point(367, 262)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(48, 17)
+        Label8.TabIndex = 7
+        Label8.Text = "Y.O.B"
+        ' 
+        ' txtName
+        ' 
+        txtName.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        txtName.Location = New Point(100, 77)
+        txtName.Name = "txtName"
+        txtName.Size = New Size(100, 25)
+        txtName.TabIndex = 8
+        ' 
+        ' txtPhone
+        ' 
+        txtPhone.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        txtPhone.Location = New Point(100, 198)
+        txtPhone.Name = "txtPhone"
+        txtPhone.Size = New Size(100, 25)
+        txtPhone.TabIndex = 9
+        ' 
+        ' txtEmail
+        ' 
+        txtEmail.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        txtEmail.Location = New Point(100, 254)
+        txtEmail.Name = "txtEmail"
+        txtEmail.Size = New Size(100, 25)
+        txtEmail.TabIndex = 10
+        ' 
+        ' txtReg
+        ' 
+        txtReg.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        txtReg.Location = New Point(436, 77)
+        txtReg.Name = "txtReg"
+        txtReg.Size = New Size(100, 25)
+        txtReg.TabIndex = 11
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Location = New Point(436, 198)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(121, 25)
+        ComboBox2.TabIndex = 15
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        RadioButton1.Location = New Point(102, 145)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(60, 21)
+        RadioButton1.TabIndex = 16
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Male"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        RadioButton2.Location = New Point(159, 144)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(74, 21)
+        RadioButton2.TabIndex = 17
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Female"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Font = New Font("Times New Roman", 11.25F, FontStyle.Bold)
+        DateTimePicker1.Location = New Point(434, 256)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(123, 25)
+        DateTimePicker1.TabIndex = 18
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(436, 322)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(114, 23)
+        Button1.TabIndex = 19
+        Button1.Text = "Register"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(DateTimePicker1)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(ComboBox2)
+        Controls.Add(txtReg)
+        Controls.Add(txtEmail)
+        Controls.Add(txtPhone)
+        Controls.Add(txtName)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Registration"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtPhone As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents txtReg As TextBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Button1 As Button
+
+End Class
